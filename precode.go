2c9a8f021563e433f653a036080e60ec1422fa18
Alex Nuka <alex.nuka@yandex.ru>
2023-12-18 20:01:52 +0300

    попытка 2

--- precode.go
+++ precode.go
@@ -57,11 +57,43 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 
 	handler.ServeHTTP(responseRecorder, req)
 
-	require.Equal(t, http.StatusOK, responseRecorder.Code, "Код ответа должен быть 200")
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "Код ответа  200")
 	require.NotEmpty(t, responseRecorder.Body.String(), "Тело ответа не должно быть пустым")
 
 	responseBody := responseRecorder.Body.String()
 	cafes := strings.Split(responseBody, ",")
 
 	require.Len(t, cafes, 4, "Количество возвращенных кафе должно быть равно 4")
+}
+
+func TestMainHandlerWhenUnsupportedCity(t *testing.T) {
+	req, err := http.NewRequest("GET", "/cafe?count=3&city=unsupported", nil)
+	require.NoError(t, err, "Ошибка при создании запроса")
+
+	responseRecorder := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code, "Код ответа 400")
+	require.Contains(t, responseRecorder.Body.String(), "wrong city value", "Тело ответа содержать ошибку wrong city value!!!!")
+}
+
+func TestMainHandlerWhenCountExceedsTotal(t *testing.T) {
+	totalCount := 4
+	req, err := http.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+	require.NoError(t, err, "Ошибка при создании запроса")
+
+	responseRecorder := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "Код ответа  200")
+	require.NotEmpty(t, responseRecorder.Body.String(), "Тело ответа не должно быть пустым")
+
+	responseBody := responseRecorder.Body.String()
+	cafes := strings.Split(responseBody, ",")
+
+	require.Len(t, cafes, totalCount, "Количество возвращенных кафе должно равно общему количеству доступных кафе")
 }
